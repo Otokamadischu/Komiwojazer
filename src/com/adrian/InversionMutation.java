8f839436e16cc2589fc11f5bbab6869bd99d9768
Otokamadischu <m4ster@op.pl>
2020-07-19 19:18:12 +0200

    Added mutation to invert oder of cities in total roads.

---
+++ src/com/adrian/InversionMutation.java
@@ -0,0 +1,33 @@
+package com.adrian;
+
+import java.util.Random;
+
+public class InversionMutation implements Mutation {
+
+	public int[][] mutate(int[][] randomRoads){
+		
+		int length = randomRoads.length;
+		int start;
+		int end;
+		int tmp;
+		
+		Random r = new Random();
+		
+		for (int x=0; x<length-1; x++) 
+		{
+			start = r.nextInt(length -2);
+			end = r.nextInt(length - start -1);
+			end += start+1;
+
+			for (int y = start; y <= end; y++) {
+				
+				tmp = randomRoads[x][y];
+				randomRoads[x][y] = randomRoads[x][end];
+				randomRoads[x][end] = tmp;
+				if(end>0)
+					end--;
+			}
+		}
+		return randomRoads;
+	}
+}
